0b08b01b62568aabac25562204c7e70e53588b54
natanzeraa <contato@natanfelipe.com.br>
2025-07-13 11:07:58 -0300

    feat: add bottom navigation to content view

--- FlashCardApp/ContentView.swift
+++ FlashCardApp/ContentView.swift
@@ -1,61 +1,27 @@
-//
-//  ContentView.swift
-//  FlashCardApp
-//
-//  Created by Natan Oliveira on 13/07/25.
-//
-
 import SwiftUI
 import SwiftData
 
 struct ContentView: View {
     @Environment(\.modelContext) private var modelContext
-    @Query private var items: [Item]
 
     var body: some View {
-        NavigationSplitView {
-            List {
-                ForEach(items) { item in
-                    NavigationLink {
-                        Text("Item at \(item.timestamp, format: Date.FormatStyle(date: .numeric, time: .standard))")
-                    } label: {
-                        Text(item.timestamp, format: Date.FormatStyle(date: .numeric, time: .standard))
-                    }
+        TabView {
+            DeckView()
+                .tabItem {
+                    Label("My decks", systemImage: "rectangle.stack")
                 }
-                .onDelete(perform: deleteItems)
-            }
-            .toolbar {
-                ToolbarItem(placement: .navigationBarTrailing) {
-                    EditButton()
+            AddNewFlashCardView()
+                .tabItem {
+                    Label("Add", systemImage: "plus")
                 }
-                ToolbarItem {
-                    Button(action: addItem) {
-                        Label("Add Item", systemImage: "plus")
-                    }
+            UserAccountView()
+                .tabItem {
+                    Label("MyAccount", systemImage: "person")
                 }
-            }
-        } detail: {
-            Text("Select an item")
-        }
-    }
-
-    private func addItem() {
-        withAnimation {
-            let newItem = Item(timestamp: Date())
-            modelContext.insert(newItem)
-        }
-    }
-
-    private func deleteItems(offsets: IndexSet) {
-        withAnimation {
-            for index in offsets {
-                modelContext.delete(items[index])
-            }
         }
     }
 }
 
 #Preview {
     ContentView()
-        .modelContainer(for: Item.self, inMemory: true)
 }
---
+++ FlashCardApp/Views/FlashCardsView.swift
@@ -0,0 +1,7 @@
+//
+//  FlashCardsView.swift
+//  FlashCardApp
+//
+//  Created by Natan Oliveira on 13/07/25.
+//
+
