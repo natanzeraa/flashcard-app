a6914c224e469c546eacb8b9e9262d9e9e43f6bb
natanzeraa <contato@natanfelipe.com.br>
2025-07-19 16:43:29 -0300

    feat: update color extension for using lighter colors in dark theme and darker colors in light theme

--- FlashCardApp/Extensions/RandomColorExtension.swift
+++ FlashCardApp/Extensions/RandomColorExtension.swift
@@ -1,11 +1,24 @@
 import SwiftUI
 
 extension Color {
-    static var random: Color {
-        Color(
-            red: .random(in: 0.2...1),
-            green: .random(in: 0.2...1),
-            blue: .random(in: 0.2...1)
-        )
+    static func smartRandom(for colorScheme: ColorScheme) -> Color {
+        switch colorScheme {
+        case .dark:
+            // Em temas escuros, use cores mais claras
+            return Color(
+                red: .random(in: 0.7...1),
+                green: .random(in: 0.7...1),
+                blue: .random(in: 0.7...1)
+            )
+        case .light:
+            // Em temas claros, use cores mais escuras
+            return Color(
+                red: .random(in: 0.2...0.5),
+                green: .random(in: 0.2...0.5),
+                blue: .random(in: 0.2...0.5)
+            )
+        @unknown default:
+            return .gray
+        }
     }
 }
