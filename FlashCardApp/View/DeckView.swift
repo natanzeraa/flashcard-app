d37dcedc9ebfc3dd6541c490f92d92fa2e05bc7a
natanzeraa <contato@natanfelipe.com.br>
2025-07-19 16:45:18 -0300

    feat: update deck view with pills and new button for adding cards

--- FlashCardApp/View/DeckView.swift
+++ FlashCardApp/View/DeckView.swift
@@ -6,19 +6,42 @@ struct DeckStruct: Identifiable {
     let caption: String
 }
 
-struct DeckCard: View {
-    let deck: DeckStruct
+struct PillStruct: Identifiable {
+    let id = UUID()
+    let title: String
+}
+
+struct Pill: View {
+    let pill: PillStruct
+    @Environment(\.colorScheme) var scheme
 
     var body: some View {
-        VStack(alignment: .center, spacing: 10) {
-            Text(deck.title)
-                .font(.headline)
-                .multilineTextAlignment(.center)
-                .colorInvert()
-                .lineLimit(1)
-                .frame(maxWidth: .infinity, alignment: .leading)
-                .truncationMode(.tail)
+        Text(pill.title)
+            .pillStyle(color: Color.smartRandom(for: scheme))
+    }
+}
 
+struct DeckCard: View {
+    let deck: DeckStruct
+    @Environment(\.colorScheme) var scheme
+    
+    var body: some View {
+        VStack(alignment: .center, spacing: 10) {
+            
+            HStack {
+                Text(deck.title)
+                    .font(.headline)
+                    .multilineTextAlignment(.center)
+                    .colorInvert()
+                    .lineLimit(1)
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                    .truncationMode(.tail)
+                
+                Text("5 perguntas")
+                    .font(.subheadline)
+                    .italic()
+                    .colorInvert()
+            }
 
             Text(deck.caption)
                 .font(.subheadline)
@@ -30,7 +53,7 @@ struct DeckCard: View {
         }
         .padding()
         .frame(maxWidth: .infinity)
-        .background(Color.random)
+        .background(Color.smartRandom(for: scheme))
         .cornerRadius(12)
         .shadow(radius: 5)
         .padding(.horizontal)
@@ -38,27 +61,49 @@ struct DeckCard: View {
 }
 
 struct DeckView: View {
-    
-    let cardMap: [DeckStruct] = [
-        DeckStruct(title: "Inglês", caption: "Este deck contém flashcards relacionados ao aprendizado do idioma inglês."),
-        DeckStruct(title: "Swift", caption: "Flashcards sobre desenvolvimento iOS com Swift."),
-        DeckStruct(title: "Segurança", caption: "Pratique conceitos de cibersegurança.")
-    ]
-    
+    @StateObject private var viewModel = DeckViewModel()
+    @State private var isNavigating = false
+
     var body: some View {
-        VStack {
-            HStack {
-                Text("My Decks")
-                    .bold()
-                    .font(Font.title2)
-                Text("? Decks")
-                    .font(Font.caption)
+        NavigationStack {
+            VStack {
+                HStack {
+                    Text("My Decks")
+                        .bold()
+                        .font(Font.title2)
+                
+                    Text("\(viewModel.decks.count) Decks")
+                        .font(Font.caption)
+                
+                    Spacer()
+                
+                    Button(action: { isNavigating = true } ) {
+                        Image(systemName: "plus")
+                            .font(.title2)
+                            .padding(8)
+                            .background(Color.blue.opacity(0.1))
+                            .clipShape(Circle())
+                    }
+                }
+                .navigationDestination(isPresented: $isNavigating) {
+                    AddNewFlashCardView()
+                }
             }
+            .frame(maxWidth: .infinity, alignment: .leading)
             .padding(.horizontal)
+            
+            ScrollView(.horizontal, showsIndicators: false) {
+                HStack(spacing: 8) {
+                    ForEach(viewModel.pills) { pill in
+                        Pill(pill: pill)
+                    }
+                }
+                .padding(.horizontal)
+            }
             
             ScrollView {
                 VStack(spacing: 16) {
-                    ForEach(cardMap) { deck in
+                    ForEach(viewModel.decks) { deck in
                         DeckCard(deck: deck)
                     }
                 }
