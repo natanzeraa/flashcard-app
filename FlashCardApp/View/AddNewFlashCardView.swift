acaf398c14bff5a1ec509a9f1c5fe3c2d5197bec
natanzeraa <contato@natanfelipe.com.br>
2025-07-19 16:44:35 -0300

    feat: add new flashcard screen layout

--- FlashCardApp/View/AddNewFlashCardView.swift
+++ FlashCardApp/View/AddNewFlashCardView.swift
@@ -1,7 +1,31 @@
 import SwiftUI
 
 struct AddNewFlashCardView: View {
+    @State private var question = "Digite sua pergunta"
+    @State private var answer = "Insira sua resposta"
+    @State private var deck = "Qual deck deseja atribuir esse flashcard?"
+
     var body: some View {
-        Text("Aqui você vai adicionar um novo flashcard!")
+        VStack(alignment: .center, spacing: 10) {
+            HStack {
+                Text("Adicionar novo flashcard")
+                    .bold()
+                    .font(Font.title2)
+            }
+            .padding(.horizontal)
+            
+            ScrollView {
+                VStack(spacing: 16) {
+                    TextField("Pegunta", text: $question)
+                        .padding()
+                    TextField("Resposta", text: $answer)
+                        .padding()
+                    TextField("Deck", text: $deck)
+                        .padding()
+                }
+                .padding(.top)
+            }
+            
+        }
     }
 }
